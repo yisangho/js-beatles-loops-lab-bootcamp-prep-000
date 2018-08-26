32ed6ee45c25285097228140a84d36c2f5f6c516
yisangho <yisangho717@gmail.com>
2018-08-26 03:34:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function theBeatlesPlay(arrMusicians, arrInstruments) {
   var arrTempStore = [];
 
   for(let i  = 0 ; i < arrMusicians.length ; i++) {
-    arrTempStore[i] = arrMusicians[i]
+    arrTempStore[i] = arrMusicians[i] + " plays " + arrInstruments[i]
   }
   return arrTempStore
 }
